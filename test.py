384a2aa94cf7c850dde7e0816b2911a6cd9d4e04
Baldribert <baldribert1@gmail.com>
2020-02-21 18:07:00 +0100

    deleted code with ggplot, not working. Get damaged PDF. TODO figure out how to print to console

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-from rpy2.robjects import pandas2ri
-pandas2ri.activate()
-from rpy2.robjects import FloatVector
+from rpy2.robjects.vectors import FloatVector
 from rpy2.robjects.packages import importr
+from rpy2 import robjects
 from rpy2.robjects.vectors import StrVector
 
-# packnames = ('mcr', 'ggplot2')
-#
-# utils = importr("utils")
-#
-# utils.chooseCRANmirror(ind=1)
-#
-# utils.install_packages(StrVector(packnames))
+
+packnames = ('mcr', 'ggplot2')
+
+utils = importr("utils")
+
+utils.chooseCRANmirror(ind=1)
+
+utils.install_packages(StrVector(packnames))
 
 mcr = importr('mcr')
 
@@ -24,44 +24,15 @@ y = FloatVector([1,2,3,4,5,6,7,8,9])
 #sample_data = pd.DataFrame(data=d)
 #rdf = com.convert_to_r_dataframe(sample_data)
 
+dem = robjects.r('reg = "Deming"')
+
+result = mcr.mcreg(x, y, dem)
+
+print(mcr.plot(result))
 
 
-result = mcr.mcreg(x,y)
+#print(robjects.r(res))
 
-plot_res = mcr.plot(result)
-
-print(plot_res)
-
-from rpy2 import robjects
-from rpy2.robjects import Formula, Environment
-from rpy2.robjects.vectors import IntVector, FloatVector
-from rpy2.robjects.lib import grid
-from rpy2.robjects.packages import importr, data
-from rpy2.rinterface import RRuntimeError
-import warnings
-
-# The R 'print' function
-rprint = robjects.globalenv.get("print")
-stats = importr('stats')
-grdevices = importr('grDevices')
-base = importr('base')
-datasets = importr('datasets')
-
-grid.activate()
-
-import math, datetime
-import rpy2.robjects.lib.ggplot2 as ggplot2
-import rpy2.robjects as ro
-from rpy2.robjects.packages import importr
-base = importr('base')
-
-mtcars = data(datasets).fetch('mtcars')['mtcars']
-
-pp = ggplot2.ggplot(mtcars) + \
-     ggplot2.aes_string(x='wt', y='mpg', col='factor(cyl)') + \
-     ggplot2.geom_point() + \
-     ggplot2.geom_smooth(ggplot2.aes_string(group = 'cyl'),
-                         method = 'lm')
-pp.plot()
+#print(plot_res)
 
 
