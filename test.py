365abec7182eb422630fdd9cb00952cb27bb9d38
Baldribert <baldribert1@gmail.com>
2020-02-27 21:28:20 +0100

    added the result and get it to work

--- test.py
+++ test.py
@@ -3,36 +3,31 @@ from rpy2.robjects.packages import importr
 from rpy2 import robjects
 from rpy2.robjects.vectors import StrVector
 
-
 packnames = ('mcr', 'ggplot2')
 
 utils = importr("utils")
 
 utils.chooseCRANmirror(ind=1)
 
-utils.install_packages(StrVector(packnames))
+#utils.install_packages(StrVector(packnames))
 
 mcr = importr('mcr')
-
+r = robjects.r
 #mcr = import_packages(packnames = "mcr", mirror = "CRAN")
 
 x = FloatVector([1,2,3,4,5,6,7,8,9])
 y = FloatVector([1,2,3,4,5,6,7,8,9])
 
+rprint = robjects.r["print"]
 
-#d = {'ID1': [1, 2, 3, 4, 5, 6], 'ID2': [1, 2, 3, 4, 5, 6]}
-#sample_data = pd.DataFrame(data=d)
-#rdf = com.convert_to_r_dataframe(sample_data)
+rmcreg = robjects.r['mcreg']
+rplot = robjects.r['plot']
 
-dem = robjects.r('reg = "Deming"')
+#dem = robjects.r('reg = "Deming"')
 
-result = mcr.mcreg(x, y, dem)
+result = rmcreg(x, y)
 
-print(mcr.plot(result))
+print(result)
 
-
-#print(robjects.r(res))
-
-#print(plot_res)
 
 
